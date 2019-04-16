9edf2740831523f8da249fc0a02d2fa7f721360c
Kobie Nikka <kobienikka@gmail.com>
2019-04-16 09:44:55 -0700

    new functions
    - added an input feature
    - began to implement cipher

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plaintext: ""
+    };
+    this.updateName = this.updateName.bind(this);
+  }
+
+  updateName(event){
+    this.setState({plaintext: event.target.value})
+  }
+  
   render() {
     return(
       <div> <form> 
       Input: <br /> 
-      <input type="text" name="Input" /><br />
-      Output: <br />
-      <input type="text" name="Output" /><br />
+      <input type="text" name="Input" onChange={this.updateName}/> <br/>
+      Output: <br/>
+      <input type="text" name="Output" /> <br/>
       </form>
-      </div>);
+      {encrypt(this.state.plaintext)}
+      </div>
+    );
   }
 }   
+
+function encrypt(phrase){
+  return "a" + phrase + "a";
+}
+
+function decrypt(){
+
+}
 
 export default App;
