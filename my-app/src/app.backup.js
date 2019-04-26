d9221922a770068d696c7bb41151480200e0166c
Miles <milesmalin21@k12.xrds.org>
2019-04-26 10:07:00 -0700

    back up file for js

---
+++ my-app/src/app.backup.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plaintext: ""
+    };
+    this.updateName = this.updateName.bind(this);
+  }
+
+  updateName(event){
+    this.setState({plaintext: event.target.value})
+  }
+
+  
+  render() {
+    return(
+      <div> <form> 
+      Input: <br /> 
+      <input type="text" name="Input" onChange={this.updateName}/> <br/>
+      Output: <br/>
+      <input type="text" name="Output" /> <br/>
+      </form>
+      {encrypt(this.state.plaintext)}
+      </div>
+    );
+  }
+}   
+
+function encrypt(phrase){
+  return "a" + phrase + "a";
+}
+
+function decrypt(){
+
+}
+
+export default App;
