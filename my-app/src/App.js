932845081d4f20ccde136a5959fb5d1f3e6387cf
Kobie Nikka <kobienikka@gmail.com>
2019-05-03 09:07:54 -0700

    Finished encrypt 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,67 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.encrpytLevel = 1;
-    this.state = {
-      plaintext: "",
-      cipherText: ""
-    };
-    this.updateName = this.updateName.bind(this);
-  }
-
-  updateName(event){
-      const plaintext = event.target.value;
-      const cipherText = encrypt1(plaintext);
-      const cipherText2 = encrypt2(plaintext);
-      this.setState({plaintext, cipherText});
-      this.setState({plaintext, cipherText2});
-  }
-  
-  // changeEncryptLevel(){
-  //   if (this.encrpytLevel = 1)
-  //     this.encrpytLevel = 2
-  //   if (this.encrpytLevel = 2)
-  //     this.encrpytLevel = 1
-  //   // return this.encrpytLevel
-
-  // }
-
-  render() {
-    return(
-      <div> <form> 
-      Input: <br /> 
-      <input type="text" name="Input" onChange={this.updateName}/> <br/>
-      <br/>
-      Output Decryption 1: <br/>
-      </form>
-      Encrypted message: {this.state.cipherText} <br/>
-      Decrypted message: {decrypt1(this.state.cipherText)}
-      <br/><br/>
-      Output Decryption 2:
-       <br/>
-      Encrypted message: {this.state.cipherText2} <br/>
-      Decrypted message: {decrypt2(this.state.cipherText2)}
-      </div>
-    );
-  }
-}   
-
-function randomLetter(){
-  var randomNum = Math.floor((Math.random() * 25) + 0)
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var randLetter = alphabet[randomNum]
-  return randLetter
-}
-function randomSymbol(){
-  var randomNum1 = Math.floor((Math.random() * 25) + 0)
-  var symbols = "å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ®ß†¨√∑≈¥Ω"
-  var randsymbol = symbols[randomNum1]
-  return randsymbol
-
-}
 const NumberMapping = {
   "a": 0,
   "b": 1,
@@ -123,41 +62,111 @@ const mapping = {
   " ": " "
 }
 
-function CeasarEncrypt(phrase, shift){
-  const letters = phrase.split('');
-  let numbers = letters.map((letter) => {return NumberMapping[letter]});
-  let shiftedNumbers = numbers.map((number) => {return (number + shift) % 26});
-  var alphabet = "å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ®ß†¨√∑≈¥Ω";
-  let NewLetters = shiftedNumbers.map((number) => {return alphabet[number]});
-  return NewLetters;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.encrpytLevel = 1;
+    this.state = {
+      plaintext: "",
+      cipherText: ""
+    };
+    this.updateName = this.updateName.bind(this);
+    this.randomLetter = this.randomLetter.bind(this);
+    this.randomSymbol = this.randomSymbol.bind(this);
+    this.CeasarEncrypt = this.CeasarEncrypt.bind(this);
+    this.encrypt1 = this.encrypt1.bind(this);
+    this.decrypt1 = this.decrypt1.bind(this);
+    this.encrypt2 = this.encrypt2.bind(this);
+    this.decrypt2 = this.decrypt2.bind(this);
+  }
 
-}
+  updateName(event){
+      const plaintext = event.target.value;
+      const cipherText = this.encrypt1(plaintext);
+      const cipherText2 = this.encrypt2(plaintext);
+      this.setState({plaintext, cipherText});
+      this.setState({plaintext, cipherText2});
+  }
   
-function encrypt1(phrase){
-  const letters = phrase.split('');
-  let cipherText = '';
-  letters.forEach((letter) => {cipherText += randomLetter() + letter} )
-  return cipherText.toUpperCase();
+  // changeEncryptLevel(){
+  //   if (this.encrpytLevel = 1)
+  //     this.encrpytLevel = 2
+  //   if (this.encrpytLevel = 2)
+  //     this.encrpytLevel = 1
+  //   // return this.encrpytLevel
+
+  // }
+
+  render() {
+    return(
+      <div> <form> 
+      Input: <br /> 
+      <input type="text" name="Input" onChange={this.updateName}/> <br/>
+      <br/>
+      Output Decryption 1: <br/>
+      </form>
+      Encrypted message: {this.state.cipherText} <br/>
+      Decrypted message: {this.decrypt1(this.state.cipherText)}
+      <br/><br/>
+      Output Decryption 2:
+       <br/>
+      Encrypted message: {this.state.cipherText2} <br/>
+      Decrypted message: {this.decrypt2(this.state.cipherText2)}
+      </div>
+    );
+  }  
+
+  randomLetter(){
+    var randomNum = Math.floor((Math.random() * 25) + 0)
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var randLetter = alphabet[randomNum]
+    return randLetter
+  }
+
+  randomSymbol() {
+    var randomNum1 = Math.floor((Math.random() * 25) + 0);
+    var symbols = "å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ®ß†¨√∑≈¥Ω";
+    var randsymbol = symbols[randomNum1];
+    return randsymbol;
+  }
+
+
+  CeasarEncrypt(phrase, shift){
+    const letters = phrase.split('');
+    let numbers = letters.map((letter) => {return NumberMapping[letter]});
+    let shiftedNumbers = numbers.map((number) => {return (number + shift) % 26});
+    var alphabet = "å∫ç∂´ƒ©˙ˆ∆˚¬µ˜øπœ®ß†¨√∑≈¥Ω";
+    let NewLetters = shiftedNumbers.map((number) => {return alphabet[number]});
+    return NewLetters;
+
+  }
+    
+  encrypt1(phrase){
+    const letters = phrase.split('');
+    let cipherText = '';
+    letters.forEach((letter) => {cipherText += this.randomLetter() + letter} )
+    return cipherText.toUpperCase();
+  }
+
+  decrypt1(phrase){
+    const newLetters = phrase.split('');
+    let answer = '';
+    newLetters.forEach((letter, i) => {if (i % 2 != 0) answer += letter})
+    return answer;
+  }
+
+  encrypt2(phrase){
+    let cipherText2 = '';
+    this.CeasarEncrypt(phrase.toLowerCase(), this.state.plaintext.length).forEach((letter) => {cipherText2 += this.randomSymbol() + letter} );
+    return cipherText2;
+  }
+
+  decrypt2(phrase){
+
+  }
 }
 
-function decrypt1(phrase){
-const newLetters = phrase.split('');
-let answer = '';
-newLetters.forEach((letter, i) => {if (i % 2 != 0) answer += letter})
-return answer;
-}
-
-function encrypt2(phrase){
-  let cipherText2 = '';
-  CeasarEncrypt(phrase.toLowerCase(), 3).forEach((letter) => {cipherText2 += randomSymbol() + letter} );
-  return cipherText2;
-}
-
-function decrypt2(phrase){
-
-}
-
-export default App;
+  export default App;
 
 
 
